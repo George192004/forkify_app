5b26f255540a05773db736bce53375fcc1179c99
George192004 <giorgiqajaia173@yahoo.com>
2023-11-13 19:22:47 +0400

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '7036344e-c32b-4b31-bf0d-d92e7954db65';
+export const MODAL_CLOSE_SEC = 2.5;
